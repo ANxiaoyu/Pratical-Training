13fe362139cbde8202bde326ffd3f684397b1f5c
ANxiaoyu <2587421590@qq.com>
2019-08-08 22:21:27 +0800

    15 commit

--- jQuery开发商品展示及购物车/js/index.js
+++ jQuery开发商品展示及购物车/js/index.js
@@ -1,25 +1,116 @@
-// 1 实现从本地加载页面
-// 向左侧选择商品列表中添加商品
-function appendProductToCart(productImage, productName, productPrice) {
-    var productItem = '<li>'+'<img src="'+productImage+'" alt="">'+'<span class="shan">'+productName+'</span>'+'<span class="price">'+productPrice+'</span>';
-
-    // 添加已经选择商品
-    var cart_ul = document.getElementById('cart');
-    $('#cart').append($(productItem));
+// 1 实现商品显示动画效果
+// 商品 hover 效果
+function productHoverEffect() {
+    $('#grid .product .product-detail').hover(
+        function(){
+            $(this).addClass('animate');
+        },function(){
+            $(this).removeClass('animate');
+        }
+    )
 }
-// 从本地加载已选择的商品数据
-function loadProductsLocalStorage() {
-    var productsJson = localStorage.getItem('products');
-    console.log(productsJson);
-    var products = [];
-    
-    if(productsJson !== '' && productsJson !== null && productsJson !== undefined) {
-        products = JSON.parse(productsJson);
-        showCheckoutBtn();
 
-        $.each(products, function(i, v) {
-            appendProductToCart(v.image, v.name, v.price);
-        });
+// 2 实现商品列表布局切换功能
+function switchProductLayout() {
+    // 2.1 large grid点击事件
+    $('#gridSelector').on('click', '.large-grid', function(){
+        $(this).children('a').addClass('active');
+        $('#gridSelector').find('.small-grid a').removeClass('active');
+        $('#grid').find('.product').addClass('large');
+        // 延时执行以下代码
+        setInterval(function(){
+            $('#grid').find('.info-large').show();
+        },500)
+        $('#grid').find('.product-detail .stats').hide();
+        // off解绑事件
+        $('#grid .product .product-detail').off('mouseenter');
+    })
+
+    // 2.2 small grid点击事件
+    $('#gridSelector').on('click', '.small-grid', function(){
+        $(this).children('a').addClass('active');
+        $('#gridSelector').find('.large-grid a').removeClass('active');
+        $('#grid').find('.product').removeClass('large');
+        $('#grid').find('.product-detail .stats').show();
+        $('#grid').find('.info-large').hide();
+        // 重新绑定商品hover事件
+        productHoverEffect();
+    })
+}
+
+// 3 实现将商品加入购物车功能
+function handleAddProductToCart() {
+    $('#grid').on('click', '.add-product-to-cart, .add-cart-large', function(){
+        // 3.1 定位到商品卡片product-card
+        var productInfo =$(this).parents('.product').find('.product-card');
+        // 3.2 克隆一个商品卡片，位置与原来卡片一样，并加入到body中
+        var cloneProduct = productInfo.clone().css(productInfo.offset()).appendTo('body');
+        // 3.3 获得购物车对象
+        var cartTarget = $('#cart');
+        // 商品图片
+        var productImage = productInfo.find('.product-img').attr('src');
+        // 商品名称
+        var productName = productInfo.find('.product-name').text();
+        // 商品单价
+        var productPrice = productInfo.find('.product-price').text();
+        // 3.4 商品卡片动画animate（），改变其left,top为购物车的left,top，设置width,height，并动态加入到购物车中
+        cloneProduct.animate({
+            left: cartTarget.offset().left,
+            top: cartTarget.offset().top + cartTarget.height(),
+            width: '50px',
+            height: '100px'
+
+        },500).fadeOut('slow', function(){
+            var productItem = 
+            '<div class="cart-item">'+
+            '<div class="img-wrap">'+
+            '<img src="'+ productImage +'" alt="">'+'</div>'+
+            '<div class="box">' + 
+            '<span class="name">'+ productName +'</span>' +
+            '<span class="total">'+ 1 +'</span>' + '</div>' +
+            '<strong class="price">'+ productPrice +'</strong>';
+            $('#cart').append($(productItem));
+
+            // 结账按钮显示
+            showCheckoutBtn();
+
+            // 购物车没有物品 隐藏
+            $('#cart').find('.empty').hide();
+            // 删除克隆的商品对象
+            cloneProduct.remove();
+
+            // 将已选商品本地存储
+            saveProductToLocalStorage();
+        })
+        
+    })
+}
+
+// 4 结账按钮显示
+function showCheckoutBtn() {
+    if(!$('#checkout').is(':visible')){
+        $('#checkout').fadeIn(500);
     }
 }
 
+// 5 实现已选商品的本地存储
+function saveProductToLocalStorage() {
+    // 5.1 循环遍历购物车中商品cart-item，得到商品的image,price,name
+    var productData = [];
+    $('#cart').find('.cart-item').each(function(i, target){
+        productData.push({
+            image:$(target).find('img').attr('src'),
+            name:$(target).find('span').text(),
+            price:$(target).find('strong').text()
+        });
+    });
+
+    localStorage.setItem('products', JSON.stringify(productData));
+}
+// 6 实现从本地加载商品数据
+
+$(function(){
+    productHoverEffect();
+    switchProductLayout();
+    handleAddProductToCart()
+})
