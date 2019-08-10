9046264c303ab61fb3ccc8d2e05832ee1cf0a592
ANxiaoyu <2587421590@qq.com>
2019-08-10 21:23:57 +0800

    commit

--- jQuery开发商品展示及购物车/js/cart.js
+++ jQuery开发商品展示及购物车/js/cart.js
@@ -1,14 +1,134 @@
-// 1 从本地存储中加载已经选择的商品，并初始化购物车
+// 7 从本地存储中加载已经选择的商品，并初始化购物车
 function initCartProductByLocalStorage() {
-    // 从localStorage 获取商品数据
+    // 7.1 从localStorage 获取商品数据
     var productsJson = localStorage.getItem('products');
+
+    // 获取购物车的tbody
+    var cartBody = $('#cartTable tbody');
    
     if(productsJson !== '' && productsJson !== null && productsJson !== undefined) {
         // 将JSON形式数据，转换成JS数组
         var products = JSON.parse(productsJson);
+        // 7.2 循环遍历获得的商品信息，并动态的加入到购物车上
+        $.each(products, function(i, v){
+            var productElm = 
+            '<tr>'+
+            '<td><input type="checkbox" name="productCheck"></td>'+
+            '<td class="cart-product-name">'+'<img src="'+ v.image +'" alt="No Image">'+'<span>'+ v.name +'</span>'+'</td>'+
+            '<td class="cart-product-price">'+ v.price +'</td>'+
+            '<td class="cart-product-count">'+
+                '<span class="reduce">-</span>'+
+                '<input type="text" class="count-input" value="'+ v.total +'">'+
+                '<span class="add">+</span>'+
+            '</td>'+
+            '<td class="cart-product-subtotal">'+ v.price +'</td>'+
+           ' <td class="cart-product-operation">'+
+               ' <span class="delete">删除</span>'+
+            '</td>'+
+        '</tr>';
+
+        // 将动态创建的商品信息加入到tbody中
+        cartBody.append(productElm);
+        })
+
+        // 8 计算商品金额小计
+        // 8.1 给增加add按钮注册点击事件
+        cartBody.on('click', '.add', function(){
+            // 8.1.1 获取input里面的内容，做加法
+            var input = $(this).prev();
+            var inputValue = parseInt(input.val());
+            input.val(inputValue+1);
+
+            // 8.1.2 商品小计计算 找到同一行tr中的单价、数量、总价， 总价=单价*数量
+            var cartBodyTr = $(this).parent().parent();
+            // 商品单价
+            var price = parseFloat(cartBodyTr.find('.cart-product-price').text());
+            // 商品数量
+            inputValue = cartBodyTr.find('.count-input').val();
+            var productSubtotal = cartBodyTr.find('.cart-product-subtotal');
+            productSubtotal.text(parseFloat(price * inputValue).toFixed());
+
+            countProductTotalAmountAndTotalCount();
+        })
+
+        // 8.2 给减少reduce按钮注册点击事件
+        cartBody.on('click', '.reduce', function(){
+            // 8.2.1 获取input里面的内容，做减法
+            var input = $(this).next();
+            var inputValue = parseInt(input.val());
+            inputValue = inputValue > 1 ? inputValue-1 : inputValue; 
+            input.val(inputValue);
+
+            // 8.2.2 商品小计计算 找到同一行tr中的单价、数量、总价， 总价=单价*数量
+            var cartBodyTr = $(this).parent().parent();
+            // 商品单价
+            var price = parseFloat(cartBodyTr.find('.cart-product-price').text());
+            // 商品数量
+            inputValue = cartBodyTr.find('.count-input').val();
+            var productSubtotal = cartBodyTr.find('.cart-product-subtotal');
+            productSubtotal.text(parseFloat(price * inputValue).toFixed());
+
+            countProductTotalAmountAndTotalCount();
+        });
+
+        // 9 计算商品总金额和总数量
+        // 9.1 给选择按钮注册事件
+        cartBody.on('click', 'input:checkbox', function(){
+            countProductTotalAmountAndTotalCount();
+        });
+        // 9.2 给全选按钮注册事件
+        $('#selectAllProduct').click(function(){
+            // prop()设置属性
+            var checkedAll = $(this).prop('checked');
+            $('#cartTable tbody :checkbox').prop('checked', checkedAll);
+            countProductTotalAmountAndTotalCount();
+        });
+
+        // 10 给删除按钮注册事件
+        cartBody.on('click', '.delete', function(){
+            var cartBodyTr = $(this).parent().parent();
+            cartBodyTr.remove();
+
+            countProductTotalAmountAndTotalCount();
+        });
+        // 10.1 删除所有商品
+        $('#deleteAllProduct').click(function(){
+            var result = confirm('是否删除所有商品？');
+            if(result == true) {
+                // 清空列表
+               $('#cartTable tbody').empty();
+
+               // 清除本地存储
+               localStorage.removeItem('products');
+               countProductTotalAmountAndTotalCount();
+            }
+        })
     }
+}
+
+// 9 计算选中商品的总金额和总数量
+function countProductTotalAmountAndTotalCount(){
+    var selectedTotalAmount = 0;
+    var selectedTotalCount = 0;
+
+    // 遍历所有选中的商品
+    $('#cartTable tbody input:checkbox[name="productCheck"]:checked').each(function(i, v){
+        var cartBodyTr = $(this).parent().parent();
+        var productCount = cartBodyTr.find('.count-input').val();
+        var productSubtotal = cartBodyTr.find('.cart-product-subtotal').text();
+        selectedTotalCount += parseInt(productCount);
+        selectedTotalAmount += parseFloat(productSubtotal);
+    })
+
+    // 设置商品总金额
+    $('#selectedTotalAmount').text(selectedTotalAmount.toFixed(2));
+
+    // 设置商品总数量
+    $('#selectedTotalCount').text(selectedTotalCount);
 }
 
 
 
-var cartBody = $('')
+$(function(){
+    initCartProductByLocalStorage();
+})
--- jQuery开发商品展示及购物车/js/index.js
+++ jQuery开发商品展示及购物车/js/index.js
@@ -53,6 +53,7 @@ function handleAddProductToCart() {
         var productName = productInfo.find('.product-name').text();
         // 商品单价
         var productPrice = productInfo.find('.product-price').text();
+        
         // 3.4 商品卡片动画animate（），改变其left,top为购物车的left,top，设置width,height，并动态加入到购物车中
         cloneProduct.animate({
             left: cartTarget.offset().left,
@@ -61,35 +62,82 @@ function handleAddProductToCart() {
             height: '100px'
 
         },500).fadeOut('slow', function(){
-            var productItem = 
-            '<div class="cart-item">'+
-            '<div class="img-wrap">'+
-            '<img src="'+ productImage +'" alt="">'+'</div>'+
-            '<div class="box">' + 
-            '<span class="name">'+ productName +'</span>' +
-            '<span class="total">'+ 1 +'</span>' + '</div>' +
-            '<strong class="price">'+ productPrice +'</strong>';
-            $('#cart').append($(productItem));
-
+           // 3.4.1 将商品加入购物车中
+               
+                var cartName = $('#cart').find('span.name').text();
+                if(cartName !== productName) {
+                    addProductToCart(productImage, productName, productPrice);
+                }else{
+                    addTotal(productName);
+                }
+            
             // 结账按钮显示
             showCheckoutBtn();
-
-            // 购物车没有物品 隐藏
-            $('#cart').find('.empty').hide();
+        
             // 删除克隆的商品对象
             cloneProduct.remove();
-
+            
             // 将已选商品本地存储
             saveProductToLocalStorage();
         })
+
+        // 判断是否有相同的商品，如果没有则加上，如果有则数量加1  
+        //  var cartName = $('#cart').find('.cart-item span.name').text();      
+        //  if(cartName == productName ) {
+        //      addTotal(productName);
+        //      cloneProduct.animate().fadeOut().remove();
+        //  }else {
+        //      cloneProduct.animate().fadeOut();
+        //  } 
+        
+        // $('#cart').find('.cart-item').each(function(i, target){
+        //     var cartName = $(target).find('span.name').text();
+        //     var total = $(target).find('span.total');
+        //     var productTotal = parseInt(total.text()); 
+        //     if(cartName == productName) {
+        //         total.text(parseInt(productTotal + 1));
+        //         cloneProduct.animate().fadeOut().remove();
+        //     }else {
+        //         cloneProduct.animate().fadeOut();
+        //     }
+        // })
+    })
+}
+
+// 3.5 判断是否加入相同的商品，如果是相同商品 total做加法
+  function addTotal(productName) {
+     
+    $('#cart').find('.cart-item').each(function(i, target){
+         var total = $(target).find('span.total');
+         var productTotal = parseInt(total.text()); 
+         var cartName = $(target).find('span.name');
+         if(cartName == productName) {   
+             total.text(parseInt(productTotal + 1));        
+         }
         
     })
+ }
+
+// 3.4.1 将选择商品添加到购物车列表
+function addProductToCart(productImage, productName, productPrice) {
+    var productItem = 
+    '<div class="cart-item">'+
+    '<div class="img-wrap">'+
+    '<img src="'+ productImage +'" alt="">'+'</div>'+
+    '<div class="box">' + 
+    '<span class="name">'+ productName +'</span>' +
+    '<span class="total">'+ 1 +'</span>' + '</div>' +
+    '<strong class="price">'+ productPrice +'</strong>';
+    $('#cart').append($(productItem));
 }
 
 // 4 结账按钮显示
 function showCheckoutBtn() {
     if(!$('#checkout').is(':visible')){
         $('#checkout').fadeIn(500);
+
+         // 购物车没有物品 隐藏
+         $('#cart').find('.empty').hide();
     }
 }
 
@@ -100,17 +148,46 @@ function saveProductToLocalStorage() {
     $('#cart').find('.cart-item').each(function(i, target){
         productData.push({
             image:$(target).find('img').attr('src'),
-            name:$(target).find('span').text(),
-            price:$(target).find('strong').text()
+            name:$(target).find('span.name').text(),
+            price:$(target).find('strong').text(),
+            total:$(target).find('span.total').text()
         });
     });
-
+    console.log(productData);
+    // 将商品数组转化成JSON,保存在本地
     localStorage.setItem('products', JSON.stringify(productData));
 }
-// 6 实现从本地加载商品数据
+
+// 6 实现从本地加载商品数据到购物车
+function loadProductLocalStorage() {
+    // 从本地获得到了商品数据
+    var productJson = localStorage.getItem('products');
+    
+    if(productJson !== '' && productJson !== null && productJson !== undefined){
+        var products = JSON.parse(productJson);
+        $.each(products, function(i, v){
+            var productItem = 
+            '<div class="cart-item">'+
+            '<div class="img-wrap">'+
+            '<img src="'+ v.image +'" alt="">'+'</div>'+
+            '<div class="box">' + 
+            '<span class="name">'+ v.name +'</span>' +
+            '<span class="total">'+ v.total +'</span>' + '</div>' +
+            '<strong class="price">'+ v.price +'</strong>';
+            // 将选择的商品添加到购物车列表
+            $('#cart').append($(productItem));
+        })
+          // 结账按钮显示
+          showCheckoutBtn();
+          console.log(products);
+    }
+}
+
+
 
 $(function(){
     productHoverEffect();
     switchProductLayout();
-    handleAddProductToCart()
+    handleAddProductToCart();
+    loadProductLocalStorage();
 })
